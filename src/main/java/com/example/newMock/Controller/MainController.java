5040771361cce7496f3939b974877c3521d72e0e
slwddn <lera1998howard@gmail.com>
2024-06-09 20:34:48 +0400

    refactoring

--- src/main/java/com/example/newMock/Controller/MainController.java
+++ src/main/java/com/example/newMock/Controller/MainController.java
@@ -1,7 +1,5 @@
 package com.example.newMock.Controller;
 
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import com.example.newMock.Model.RequestDTO;
 import com.example.newMock.Model.ResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Random;
 
 @RestController
@@ -24,52 +23,51 @@ public class MainController {
     ObjectMapper mapper = new ObjectMapper();
 
 
- @PostMapping(
-         value = "/info/PostBalance",
-         produces = MediaType.APPLICATION_JSON_VALUE,
-         consumes = MediaType.APPLICATION_JSON_VALUE
- )
-     public Object postBalances (@RequestBody RequestDTO requestDTO) {
+    @PostMapping(
+            value = "/info/PostBalance",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Object postBalances(@RequestBody RequestDTO requestDTO) {
 
-     try {
-         String clientId = requestDTO.getClientId();
-         char firstDigit = clientId.charAt(0);
-         String rqUID = requestDTO.getRqUID();
-         String currency;
+        try {
+            String clientId = requestDTO.getClientId();
+            char firstDigit = clientId.charAt(0);
+            String rqUID = requestDTO.getRqUID();
+            String currency;
 
-         double minLimit = 0D;
-         double maxLimit;
+            double minLimit = 0D;
+            double maxLimit;
 
-         if (firstDigit == '8') {
-             maxLimit = 2000D;
-             currency = "US";
-         }
-         else if (firstDigit == '9') {
-             maxLimit = 1000D;
-             currency = "EU";
-         } else {
-             maxLimit =10000D;
-             currency = "RUB";
-         }
+            if (firstDigit == '8') {
+                maxLimit = 2000D;
+                currency = "US";
+            } else if (firstDigit == '9') {
+                maxLimit = 1000D;
+                currency = "EU";
+            } else {
+                maxLimit = 10000D;
+                currency = "RUB";
+            }
 
-         double balance = Math.floor((minLimit + new Random().nextDouble (minLimit, maxLimit)) * 100) / 100;
+            double balance = Math.floor((minLimit + new Random().nextDouble(minLimit, maxLimit)) * 100) / 100;
 
-         ResponseDTO responseDTO = new ResponseDTO();
+            ResponseDTO responseDTO = new ResponseDTO();
 
-         responseDTO.setRqUID(rqUID);
-         responseDTO.setClientId(clientId);
-         responseDTO.setAccount(requestDTO.getAccount());
-         responseDTO.setCurrency(currency);
-         responseDTO.setBalance(balance);
-         responseDTO.setMaxLimit(maxLimit);
+            responseDTO.setRqUID(rqUID);
+            responseDTO.setClientId(clientId);
+            responseDTO.setAccount(requestDTO.getAccount());
+            responseDTO.setCurrency(currency);
+            responseDTO.setBalance(balance);
+            responseDTO.setMaxLimit(maxLimit);
 
-         log.error("******* RequestDTO *******" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
-         log.error("******* ResponseDTO *******" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+            log.error("******* RequestDTO *******" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("******* ResponseDTO *******" + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
 
-         return responseDTO;
+            return responseDTO;
 
-     } catch (Exception e) {
-         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-     }
- }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
